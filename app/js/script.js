9f3edc4162b712fe2dab60d0b5dd2eead0334fe4
Øystein Håberg <islandstone89@gmail.com>
2024-10-30 20:42:09 +0100

    * Added effect on Explore button * Styled mobile nav toggle * Added js file * Started js work

---
+++ app/js/script.js
@@ -0,0 +1,14 @@
+
+let nav = document.querySelector(".primary-nav__list");
+let navToggle = document.querySelector(".mobile-nav-toggle");
+
+navToggle.addEventListener("click", () => {
+
+    if (navToggle.getAttribute("aria-expanded", "false")) {
+        navToggle.setAttribute("aria-expanded", "true")
+    }
+
+    else {
+        navToggle.setAttribute("aria-expanded", "false")
+    }
+});
