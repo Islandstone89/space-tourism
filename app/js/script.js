4c5860df169ad8b878700f541cbbf6c29ae25c4e
Øystein Håberg <islandstone89@gmail.com>
2024-11-07 23:56:18 +0100

    * Done for the day

--- app/js/script.js
+++ app/js/script.js
@@ -1,11 +1,13 @@
 
-let body = document.getElementById("homePage");
 let nav = document.querySelector(".primary-nav");
 let navList = document.querySelector(".primary-nav__list");
 let navToggle = document.querySelector(".mobile-nav-toggle");
 
 navToggle.addEventListener("click", () => {
 
+    navList.toggleAttribute("active")
+
+    /* 
     if (navToggle.getAttribute("aria-expanded", "false")) {
             navToggle.setAttribute("aria-expanded", "true")
             navList.style.display = "flex"
@@ -17,6 +19,8 @@ navToggle.addEventListener("click", () => {
             navToggle.setAttribute("aria-expanded", "false")
             navToggle.style.backgroundImage = "url('/assets/shared/icon-hamburger.svg')"
         }
-    }
+    } */
+
+}
 );
 
