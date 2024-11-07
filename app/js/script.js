396edd78fec71ed95d11af0e8bbf8388e3d21562
Øystein Håberg <islandstone89@gmail.com>
2024-11-07 23:25:44 +0100

    * added padding on logo-wrapper

--- app/js/script.js
+++ app/js/script.js
@@ -1,17 +1,22 @@
 
-let nav = document.querySelector(".primary-nav__list");
+let body = document.getElementById("homePage");
+let nav = document.querySelector(".primary-nav");
+let navList = document.querySelector(".primary-nav__list");
 let navToggle = document.querySelector(".mobile-nav-toggle");
 
 navToggle.addEventListener("click", () => {
 
     if (navToggle.getAttribute("aria-expanded", "false")) {
             navToggle.setAttribute("aria-expanded", "true")
-            nav.style.display = "flex"
+            navList.style.display = "flex"
             navToggle.style.backgroundImage = "url('/assets/shared/icon-close.svg')"
+            nav.style.backgroundColor = "hsl(0 0 100% / .05)"
     }
 
     else {
-        navToggle.setAttribute("aria-expanded", "false")
-        navToggle.style.backgroundImage = "url('/assets/shared/icon-hamburger.svg')"
+            navToggle.setAttribute("aria-expanded", "false")
+            navToggle.style.backgroundImage = "url('/assets/shared/icon-hamburger.svg')"
+        }
     }
-});
+);
+
