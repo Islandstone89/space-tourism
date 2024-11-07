43aefbe5516521472817c91932c6251b7312493d
Øystein Håberg <islandstone89@gmail.com>
2024-11-07 22:55:18 +0100

    * Experimenting with JS

--- app/js/script.js
+++ app/js/script.js
@@ -6,9 +6,12 @@ navToggle.addEventListener("click", () => {
 
     if (navToggle.getAttribute("aria-expanded", "false")) {
             navToggle.setAttribute("aria-expanded", "true")
+            nav.style.display = "flex"
+            navToggle.style.backgroundImage = "url('/assets/shared/icon-close.svg')"
     }
 
     else {
         navToggle.setAttribute("aria-expanded", "false")
+        navToggle.style.backgroundImage = "url('/assets/shared/icon-hamburger.svg')"
     }
 });
